e133b7fa73ad9a098018159d49ca517f603ba1b4
TinkGu <tlwrite@126.com>
2016-03-22 09:34:34 +0800

    debug

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var fs = require('fs');
 
-function setEjsTag(tagName, handler){
-    return function(req, res, next){
-        res.locals[tagName] = handler;
-        next();
-    }
-}
-
 function mapLibs(dirname, publicPath){
     return fs.readdirSync(dirname).map(function(lib){
         return '<script src=\"' + publicPath + lib + '\"></script>'
     }).join('\n');
 }
 
-module.exports = setEjsTag('libs', mapLibs);
+module.exports = function (dirname, publicPath){
+    return function(req, res, next) {
+        res.locals.libs = mapLibs(dirname, publicPath);
+        next();
+    }
+}
